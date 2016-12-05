241fa7b3aaaee1ffceeceea2a84fc9e747e4aeb4
Tejas Sathe <sathe.tejas@gmail.com>
2016-12-05 13:26:27 -0500

    Modified jsonify to parse directories

--- jsonify.py
+++ jsonify.py
@@ -1,24 +1,35 @@
-def jsonify(string):
-	lines = []
-	for line in string:
-		lines.append(line)
+def parse(path):
+	def jsonify(string):
+		lines = []
+		for line in string:
+			lines.append(line)
+		count = int(lines[0].split(" ")[2])
+		attr = {}
+		for i in range(1, count):
+			if i <= 9:
+				try:
+					attr[lines[4+i].split(' ')[4]] = int(lines[4+i].split(' ')[25], 16)
+				except ValueError:
+					attr[lines[4+i].split(' ')[4]] = 0
+			else:
+				try:
+					attr[lines[4+i].split(' ')[3]] = int(lines[4+i].split(' ')[26], 16)
+				except ValueError:
+					attr[lines[4+i].split(' ')[3]] = 0
+		return attr
 
-	count = int(lines[0].split(" ")[2])
-	attr = {}
-	for i in range(1, count):
-		if i <= 9:
-			try:
-				attr[lines[4+i].split(' ')[4]] = int(lines[4+i].split(' ')[25], 16)
-			except ValueError:
-				attr[lines[4+i].split(' ')[4]] = 0
-		else:
-			try:
-				attr[lines[4+i].split(' ')[3]] = int(lines[4+i].split(' ')[26], 16)
-			except ValueError:
-				attr[lines[4+i].split(' ')[3]] = 0
+	from os import listdir
+	from subprocess import Popen, PIPE
+	from json import dump
+	files = listdir(path)
+	data = {}
+	for filename in files:
+		command = "readelf -SW %s" % path+filename
+		data[filename] = jsonify(Popen(command, shell=True, stdout=PIPE).stdout)
 
-	return attr
+	dump(data, open("data.json", "w"))
 
 
 
-d = jsonify(open("attr.txt", "r"))
+from sys import argv
+parse(argv[1])

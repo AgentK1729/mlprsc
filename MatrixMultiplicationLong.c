c2f2671f9060bd4b577c5921aa7ed21365883772
Pranav Chauthaiwale <pranavchauthaiwale@gmail.com>
2016-12-05 17:01:02 -0500

    Added default code

---
+++ MatrixMultiplicationLong.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define INPUTSIZE 1000
+
+void main()
+{
+    //printf("Hello World");
+  long m, n, p, q, c, d, k, sum = 0;
+  long mat1_row, mat1_col, mat2_row, mat2_col = 0;
+  long randomNumber = 0;
+  long first[INPUTSIZE][INPUTSIZE], second[INPUTSIZE][INPUTSIZE], multiply[INPUTSIZE][INPUTSIZE];
+ 
+  mat1_row = mat1_col = mat2_row = mat2_col = INPUTSIZE;
+
+    for (c = 0; c < mat1_row; c++) {
+      for (d = 0; d < mat2_col; d++) {
+        for (k = 0; k < mat2_row; k++) {
+            randomNumber = rand() % 1000;
+          sum = sum + randomNumber * randomNumber;
+        }
+ 
+        multiply[c][d] = sum;
+        sum = 0;
+      }
+    }
+  
+}
